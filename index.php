e9fa729551bde37fec5f0f9772e297722fcedd28
Malith-ra <IT18097634@my.sliit.lk>
2021-10-02 16:16:07 +0530

    index php file added

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <title>OAuth 2.0 | Google Drive API</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/my-login.css">
+
+</head>
+<body class="my-login-page">
+  <section class="h-100">
+
+    <div class="container h-100">
+      <div class="row">
+       <div class="col-md-12">
+                        <div class="row justify-content-md-center h-100">
+                          <div class="card-wrapper header" style="margin:auto">
+                           <div class="card fat">
+
+                              <div class="card-body header">
+                              <div class="brand logoImage">
+                               <img src="img/img_logo.png">
+                                </div>
+                                 <div class="header text">
+                                <h4 class="card-title">OAuth Authorization Server Application </h4>
+                                   </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+        <div class="col-md-12">
+          <div class="row justify-content-md-center h-100">
+            <div class="card-wrapper">
+              <div class="card fat" style="margin-top:30px">
+                <div class="card-body">
+                  <h3 style="text-align: center;"> File Upload Service to your Google Drive using OAuth 2.0</h3>
+                  <p style="margin-top:20px;font-size: 15px;"> Our web application shows how to utilize the Google API Client Library for PHP to provide OAuth 2.0 authorization for Google Drive API access. Users can share specified data with an application using OAuth 2.0 while keeping their usernames, passwords, and other personal information private. This application obtains permission from users to select a file and save it straight to their Google Drives using OAuth 2.0. The Authorization Code grant type is used in this web application. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
